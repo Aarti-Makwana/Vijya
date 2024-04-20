77b659645f9a7bd01eddedf6513d189107d62835
aarti <aartimakwana2408@gmail.com>
2024-04-20 14:50:30 +0530

    Design

--- server/controller/productController.js
+++ server/controller/productController.js
@@ -5,25 +5,29 @@ export const productController = async (request, response) => {
             {
                 ratings: 5,
                 price: 1000,
-                desc: "Apple Series 4 GPS A 38 MM Space",
+                heading: "Apple Series 4 GPS A 38 MM Space",
+                description:"This watch from Apple is highly known for its features, like you can control your apple smartphone with this watch and you can do everything  you would want to do on smartwatch",
                 image:"watch1.png" 
             },
             {
                 ratings: 4,
                 price: 150,
-                desc: "iPhone",
+                heading: "iPhone",
+                description:"This iPhone from Apple is highly known for its features, like you can control your apple smartWatch with this watch and you can do everything  you would want to do on smartphone",
                 image: "watch2.png" 
             },
             {
                 ratings: 3,
                 price: 500,
-                desc: "Camera",
+                heading: "Camera",
+                description:"This Camera from Apple is highly known for its features, like you can control your apple smartWatch with this watch and you can do everything  you would want to do on smartphone",
                 image:"watch3.png" 
             },
             {
                 ratings: 2,
                 price: 15000,
-                desc: "Head Phones",
+                heading: "Head Phones",
+                description:"This Head Phones from Apple is highly known for its features, like you can control your apple smartWatch with this watch and you can do everything  you would want to do on smartphone",
                 image: "watch4.png" 
             },
         ];
